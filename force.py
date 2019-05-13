602b8d4aeea6f2d4ffbea0222465ae17e00962cb
citojay <citojaya@gmail.com>
2019-05-13 08:04:00 +0100

    working force model

--- force.py
+++ force.py
@@ -2,33 +2,37 @@ import math
 
 #Electrostatic force parameters
 Vi = 4.7 #Work function of wall (Volt)
-Vj = 4.5 #Work function of particle (Volt)
+Vj = 4.2 #Work function of particle (Volt)
 eps0 = 8.885e-12 #permitivity of air (C^2/N.m^2)
-pois = 0.5
-ymod = 2.0e6
+pois = 0.3
+ymod = 1.0e7
 dens = 890.0
-parDia = 31.0e-3
+parDia = 140.0e-6
 ha = 6.5e-20
 
-charge = -6.0e-9
+charge = 0.0e-9
 ks = 1.0e-4
-vel = 2.0
+vel = 20.0
 vDash = 0.0
 imageConst = 2.0
 Zs = 200.0e-9 
-noOfImpacts = 20
+noOfImpacts = 70
 alpha = 0.9
 emod = alpha*(1.0-pois*pois)/ymod
 k0 = imageConst*Zs/eps0/(4.0*math.pi*pow(parDia*0.5,2))
 S = 1.36*pow(emod, 2.0/5.0)*pow(dens,2.0/5.0)*pow(parDia,2)*pow(vel,4.0/5.0)
+vDash = k0*charge 
+ks = 1.8e-5/(Vi-Vj-vDash)
+k1 = ks*Zs/eps0
+Qmax = (Vi-Vj)/k0 # (Matsusaka et al, 2000)
+minGap = 1.0e-6
+print("k0 ks k1 ",k0, ks,k1)
+print("Qmax ",Qmax)
+print("Max Electrostatic Force ", Qmax*Qmax/(4.0*math.pi*eps0*minGap))
 
 surf_tens = 0.013
 liq_vol = 9.59E-212
 cont_ang =0.0
-
-ks = 1.8e-5/(Vi-Vj)
-k1 = ks*Zs/eps0
-print("ks k1",ks,k1)
 
 
 f = open("charge-impact.dat","w")
@@ -44,8 +48,8 @@ for i in range(noOfImpacts):
     gap = max(gap, 0.01e-6)
     esF = charge*charge/(4.0*math.pi*eps0*gap)
 
-    f.write(str(i+1)+" "+str(round(charge*1e9,3))+"\n")
-    print(round(charge*1e9,3))
+    f.write(str(i+1)+" "+str(round(charge*1e12,3))+"\n")
+    print(round(charge*1e12,3))
 
 
     # Vanderwaal force
@@ -62,4 +66,5 @@ for i in range(noOfImpacts):
         rStar = parDia*0.5
         capf = -2.*math.pi*rStar*surf_tens*math.cos(cont_ang)/(1.0+1.0/math.sqrt(1.0+2.*liq_vol/(math.pi*rStar*pow(separation,2))-separation))
 
+print("Maximum Vanderwaal force ", fv)
 f.close()
