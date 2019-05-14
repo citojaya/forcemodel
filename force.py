b33a35cba5d97b895c5b547b010050b42c38ed8f
citojay <citojaya@gmail.com>
2019-05-14 05:46:27 +0100

    updating force.py

--- force.py
+++ force.py
@@ -1,8 +1,8 @@
 import math
 
 #Electrostatic force parameters
-Vi = 4.7 #Work function of wall (Volt)
-Vj = 4.2 #Work function of particle (Volt)
+Vi = 5.0#Work function of wall (Volt)
+Vj = 4.9 #Work function of particle (Volt)
 eps0 = 8.885e-12 #permitivity of air (C^2/N.m^2)
 pois = 0.3
 ymod = 1.0e7
@@ -10,13 +10,13 @@ dens = 890.0
 parDia = 140.0e-6
 ha = 6.5e-20
 
-charge = 0.0e-9
+charge = -6.0e-9
 ks = 1.0e-4
-vel = 20.0
+vel = 2.0
 vDash = 0.0
 imageConst = 2.0
-Zs = 200.0e-9 
-noOfImpacts = 70
+Zs =300.0e-9 
+noOfImpacts = 80
 alpha = 0.9
 emod = alpha*(1.0-pois*pois)/ymod
 k0 = imageConst*Zs/eps0/(4.0*math.pi*pow(parDia*0.5,2))
@@ -26,18 +26,21 @@ ks = 1.8e-5/(Vi-Vj-vDash)
 k1 = ks*Zs/eps0
 Qmax = (Vi-Vj)/k0 # (Matsusaka et al, 2000)
 minGap = 1.0e-6
+print("S ",S)
 print("k0 ks k1 ",k0, ks,k1)
 print("Qmax ",Qmax)
-print("Max Electrostatic Force ", Qmax*Qmax/(4.0*math.pi*eps0*minGap))
+
 
 surf_tens = 0.013
-liq_vol = 9.59E-212
+liq_vol = 9.59E-9
 cont_ang =0.0
 
+ks = 1.0e-4
 
 f = open("charge-impact.dat","w")
-
-gap = 0
+capf = 0.0
+gap = 0.0
+fv = 0.0
 for i in range(noOfImpacts):
     #Electrostatic force
     vDash = k0*charge  
@@ -45,14 +48,15 @@ for i in range(noOfImpacts):
     deltaQ = ks*S*deltaV
     charge += deltaQ
 
-    gap = max(gap, 0.01e-6)
-    esF = charge*charge/(4.0*math.pi*eps0*gap)
+    gap = max(gap, 1e-6)
+    esF = charge*charge/(4.0*math.pi*eps0*gap*gap)
 
-    f.write(str(i+1)+" "+str(round(charge*1e12,3))+"\n")
-    print(round(charge*1e12,3))
+    f.write(str(i+1)+" "+str(round(charge*1e9,3))+"\n")
+    print(round(charge*1e9,3))
 
 
     # Vanderwaal force
+    gap = 0
     vGapMn = 1.0e-9
     gap = max(gap, vGapMn)
     fv = -ha*parDia*0.5/(6.*gap*gap)
@@ -65,6 +69,8 @@ for i in range(noOfImpacts):
         separation = max(sepMin, gap)
         rStar = parDia*0.5
         capf = -2.*math.pi*rStar*surf_tens*math.cos(cont_ang)/(1.0+1.0/math.sqrt(1.0+2.*liq_vol/(math.pi*rStar*pow(separation,2))-separation))
-
+minGap = 10.0e-6
+print("Maximum Capillary force ", capf)        
 print("Maximum Vanderwaal force ", fv)
+print("Max Electrostatic Force ", Qmax*Qmax/(4.0*math.pi*eps0*minGap*minGap))
 f.close()
